2238c4cae14ac9180e80242c88e3aedae52f73f1
Gerhard Aspeling <aspeling.gerhard@gmail.com>
2017-08-15 22:15:59 +0200

    refactor code to be more manageable and consistent

--- php/ProductController.php
+++ php/ProductController.php
@@ -10,82 +10,69 @@ use Silex\Application;
  */
 class ProductController {
 
-    /**
-     * @SLX\Route(
-     *      @SLX\Request(method="GET", uri="/{id}")
-     * )
-     * @param Application $app
-     * @param $name
-     * @return
-     */
-    public function getById_GET(Application $app, $id){
-        $curl = curl_init();
-        $requestData = array();
-        $requestData['id'] = $id;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] =$response[$i]['barcode'];
-            $prod["name"]=$response[$i]['itemName'];
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['curreny'] = $response[$i]['prices'][$j]['currencyCode'];
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }
+	/**
+	 * @SLX\Route(
+	 *      @SLX\Request(method="GET", uri="/{id}")
+	 * )
+	 * @param Application $app
+	 * @param $name
+	 * @return
+	 */
+	public function getById_GET(Application $app, $id){
+		$requestData = array();
+		$requestData['id'] = $id;
 
-        return $app->render('products/product.detail.twig', $result);
-    }
+		return $app->render('products/product.detail.twig', $result);
+	}
 
-    /**
-     * @SLX\Route(
-     *      @SLX\Request(method="GET", uri="/search/{name}")
-     * )
-     * @param Application $app
-     * @param $name
-     * @return
-     */
-    public function getByName_GET(Application $app, $name){
-        $curl = curl_init();
-        $requestData = array();
-        $requestData['names'] = $name;
-        curl_setopt($curl, CURLOPT_URL,  'http://192.168.0.241/eanlist?type=Web');
-        curl_setopt($curl, CURLOPT_POST, 1);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        $response = curl_exec($curl);
-        $response = json_decode($response);
-        curl_close($curl);
-        $result = [];
-        for ($i =0; $i < count($response) ;$i++) {
-            $prod = array();
-            $prod['ean'] = $response[$i]['barcode'];
-            $prod["name"]= $response[$i]['itemName'];
-            $prod["prices"] = array();
-            for ($j=0;$j < count($response[$i]['prices']); $j++) {
-                if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
-                    $p_price = array();
-                    $p_price['price'] = $response[$i]['prices'][$j]['sellingPrice'];
-                    $p_price['currency'] = $response[$i]['prices'][$j]['currencyCode'];
-                    $prod["prices"][] = $p_price;
-                }
-            }
-            $result[] = $prod;
-        }
+	/**
+	 * @SLX\Route(
+	 *      @SLX\Request(method="GET", uri="/search/{name}")
+	 * )
+	 * @param Application $app
+	 * @param $name
+	 * @return
+	 */
+	public function getByName_GET(Application $app, $name){
+		$requestData = array();
+		$requestData['names'] = $name;
 
-        return $app->render('products/products.twig', $result);
-    }
+		$result = $this->curlCall($requestData);
+
+		return $app->render('products/products.twig', $result);	
+	}
+
+
+	// Private function doing a curl call to 192.138.0.241/eanlist return product information
+	private function curlCall($data) {
+		$curl = curl_init();
+
+		// if this is used all over the site, I would have the $url in a config, especially if there is a dev and production environment
+		$url = 'http://192.168.0.241/eanlist?type=Web';
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_POST, 1);
+		curl_setopt($curl, CURLOPT_POSTFIELDS, $requestData);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$response = curl_exec($curl);
+		$response = json_decode($response);
+		curl_close($curl);
+
+		$result = array();
+		for ($i = 0; $i < count($response); $i++) {
+			$product = array();
+			$product['ean'] = $response[$i]['barcode'];
+			$product["name"] = $response[$i]['itemName'];
+			$product["prices"] = array();
+			for ($j = 0; $j < count($response[$i]['prices']); $j++) {
+				if ($response[$i]['prices'][$j]['currencyCode'] != 'ZAR') {
+					$productPrice = array();
+					$productPrice['price'] = $response[$i]['prices'][$j]['sellingPrice'];
+					$productPrice['currency'] = $response[$i]['prices'][$j]['currencyCode'];
+					$product["prices"][] = $productPrice;
+				}
+			}
+			$result[] = $product;
+		}
+	}
 
 }
